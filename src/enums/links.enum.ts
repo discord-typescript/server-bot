bb5df7481bf0cede93d559447c6e024b53bc4701
SteeledSlagle13 <SteeledSlagle13@gmail.com>
2021-04-12 13:47:02 -0500

    [Help Command][Shard Command] Added shard and help commands

--- src/enums/links.enum.ts
+++ src/enums/links.enum.ts
@@ -3,6 +3,9 @@ export enum LINK {
   SIMPLE_STARTER_README = "https://github.com/OwenCalvin/discord.ts/tree/master/starter-projects/simple-bot#simple-bot",
   SIMPLE_STARTER_DOWNLOAD = "https://github.com/OwenCalvin/discord.ts/raw/master/starter-projects/simple-bot/SimpleBot.zip",
 
+  SHARDING_GUIDE = "https://github.com/OwenCalvin/discord.ts/blob/master/guides/sharding.md",
+  DISCORD_JS_SHARDING = "https://discordjs.guide/sharding/#when-to-shard",
+
   DISCORD_JS_DOCS = "https://discord.js.org/?source=post_page---------------------------#/docs/main/stable/general/welcome",
 
   INVITE_LINK = "https://discord.gg/VDjwu8E",
